77976c116d005aeb5b3807e377458817506a6386
I was eaten by a grue <iwaseatenbyagrue@gmail.com>
2017-10-18 19:21:09 +0000

    Added pytest-runner to setup.py test dependences.

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ setup(
     version='0.1',
     packages=find_packages(exclude='tests'),
     include_package_data=True,
+    test_runner="test",
     install_requires=[
        "Click",
         "click-plugins",
         "marshmallow",
-        
     ],
-    extras_require={u'dev': [u'pytest', u'pytest-pep8', u'pytest-cov']},
+    extras_require={u'tests': [u'pytest', u'pytest-runner', u'pytest-pep8', u'pytest-cov']},
     entry_points='''
 [console_scripts]
 tfat=tfat.cli:base
