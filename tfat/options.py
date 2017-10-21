1a4b587b7db7028c2168b74dd178af8ed9e2af65
I was eaten by a grue <iwaseatenbyagrue@gmail.com>
2017-10-21 12:34:06 +0000

    Added debug_option to tfat.options, which sets tfat's global/base logger's level to debug.

--- tfat/options.py
+++ tfat/options.py
@@ -2,6 +2,8 @@ import click
 import yaml
 
 from multiprocessing import cpu_count
+from tfat.logger import logger
+
 
 def config_option(*args, **kwargs):
 
@@ -27,7 +29,7 @@ def config_option(*args, **kwargs):
 def root_dir_option(*args, **kwargs):
 
     attrs = {
-        "help": "Root directory",
+        "help": "Root directory (default is current directory)",
         "type": click.Path(resolve_path=True, file_okay=False),
         "default": "."
     }
@@ -44,10 +46,34 @@ def threads_option(*args, **kwargs):
     attrs = {
         "help": "Number of threads to use",
         "type": int,
-        "default": max(1, cpu_count()/2)
+        "default": max(1, cpu_count()/2),
+        "show_default": True
     }
 
     if not args and attrs.get('cls', click.Option) == click.Option:
         args = ["-t", "--threads"]
 
     return click.option(*args, **attrs)
+
+
+
+def debug_option(*args, **kwargs):
+
+    attrs = {
+        "help": "Enable debug logging",
+        "is_flag": True,
+    }
+
+    def set_tfat_logging(ctx, param, value):
+        if not value or ctx.resilient_parsing:
+            return
+
+        if param == "debug":
+            logger.setLevel(-1)
+        return value
+
+    if not args and attrs.get('cls', click.Option) == click.Option:
+        args = ["-D", "--debug"]
+
+    return click.option(*args, **attrs)
+
