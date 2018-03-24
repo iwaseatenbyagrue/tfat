c073468afe14e6ec120f308c6fe98a0c0cf791fe
I was eaten by a grue <iwaseatenbyagrue@gmail.com>
2018-03-24 09:59:36 +0000

    Added cli_utils to tfat, providing AliasedGroup, a click.Group subclass supporting shortened names for commands.

---
+++ tfat/cli_utils.py
@@ -0,0 +1,22 @@
+import click
+
+class AliasedGroup(click.Group):
+    """
+    A simple click.Group subclass that allows running commands using
+    a shortened form (e.g. first or first couple of letters.
+
+    Taken from:
+    http://click.pocoo.org/5/advanced/#command-aliases
+    """
+
+    def get_command(self, ctx, cmd_name):
+        rv = click.Group.get_command(self, ctx, cmd_name)
+        if rv is not None:
+            return rv
+        matches = [x for x in self.list_commands(ctx)
+                   if x.startswith(cmd_name)]
+        if not matches:
+            return None
+        elif len(matches) == 1:
+            return click.Group.get_command(self, ctx, matches[0])
+        ctx.fail('Too many matches: %s' % ', '.join(sorted(matches)))
